9e503ed909cad29efe9e6283ef7f53f8e3b28f6b
abdimoha-dev <abdymohammed@gmail.com>
2022-03-03 18:37:49 +0300

    add registration template

--- client_registry/urls.py
+++ client_registry/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('home',views.dashboard, name='home'),
+    path('client/add/client', views.new_client, name="add_new_client")
 ]
--- client_registry/views.py
+++ client_registry/views.py
@@ -5,3 +5,10 @@ def dashboard(request):
     if request.method=='GET':
         return render(request,'base.html')
     
+
+def new_client(request):
+    if request.method=='GET':
+        return render(request,'new_client.html')
+
+    else:
+        pass
